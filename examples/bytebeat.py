f466998d2ad6b4fa46f63473a1f96cc2ad2d9018
Noah Weninger <nweninge@ualberta.ca>
2016-12-24 18:07:42 -0700

    Fix divide by zero

--- examples/bytebeat.py
+++ examples/bytebeat.py
@@ -5,6 +5,6 @@ time = 0
 def process(_, output):
     global time
     t = np.arange(output.shape[1], dtype=np.int32) + time
-    samples = t / (1 + t % (1 + t >> 9 | t >> 13))
+    samples = t / (1 + t % (1 + (t >> 9 | t >> 13)))
     output[:] = (samples % 256) / 128 - 1
     time += output.shape[1]
